f210849d2ba08db00eea413f9d7cba7bde2fa499
Kovalsky95 <diana1997525@gmail.com>
2020-11-17 20:32:14 +0800

    PenguinL

--- app_run.sh
+++ app_run.sh
@@ -1,2 +1,2 @@
-javac -sourcepath ./src -d bin com/released2/calculator/main/CalcGUI.java
+javac -sourcepath ./src -d bin src/com/released2/calculator/CalcGUI.java
 java -classpath ./bin com.released2.calculator.CalcGUI
